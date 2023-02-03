4acf6d4f8f4da3c8b2f3fb8586aa3051bf62d2f0
vini00784 <vo70346@gmail.com>
2023-02-03 10:20:22 -0300

    preparing development environment

---
+++ app.js
@@ -0,0 +1,44 @@
+/*
+    Objetivo:     API responsible for handling Back-End data
+                  (GET, POST, PUT, DELETE)
+    Autor:        Vinícius Santos Oliveira
+    Data Criação: 03/02/2023
+    Versão:       1.0
+    
+    Anotações:
+    Comandos a serem rodados (nessa sequência) além do express, cors e body-parser:
+        O prisma é necessário para a manipulação do acesso ao Banco de Dados
+            npm install prisma --save
+            npx prisma
+            npx prisma init
+            npx install @prisma/client
+                Eles preparam o ambiente de utilização do Prisma
+                npx prisma migrate dev --> comando que faz uma ligação do prisma com o banco, é feito para testar se o prisma realmente consegue acessar o banco
+                    É importante rodar esse comando no início do projeto para ver se está dando tudo certo
+                    O sucesso da criação de uma tabela de teste mostra que a interação com o banco está acontecendo.
+                        OBS.: Esse comando de teste limpa tudo o que tem no banco, por isso ele deve ser rodada NO INÍCIO DO PROJETO!!!
+*/
+
+// Importing libraries
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+// Cors configuration to release API access
+app.use((request, response, next) => {
+    response.header('Access-Control-Allow-Origin', '*')
+    response.header('Access-Control-Allow-Methos', 'GET, POST, PUT, DELETE, OPTIONS')
+
+    app.use(cors())
+    next()
+})
+
+// Creating an object that allows you to receive a JSON in the body of requests
+const jsonParser = bodyParser.json()
+
+// Enable the server to receive http requests
+app.listen(3030, () => {
+    console.log('Server waiting for requests...');
+})
