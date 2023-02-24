b55385520fdd78df38b151e1fa4434b853f4f6c7
vini00784 <vo70346@gmail.com>
2023-02-24 11:05:18 -0300

    code refactoring

--- src/app.js
+++ src/app.js
@@ -4,19 +4,6 @@
     Autor:        Vinícius Santos Oliveira
     Data Criação: 03/02/2023
     Versão:       1.0
-    
-    Anotações:
-    Comandos a serem rodados (nessa sequência) além do express, cors e body-parser:
-        O prisma é necessário para a manipulação do acesso ao Banco de Dados
-            npm install prisma --save
-            npx prisma
-            npx prisma init
-            npx install @prisma/client
-                Eles preparam o ambiente de utilização do Prisma
-                npx prisma migrate dev --> comando que faz uma ligação do prisma com o banco, é feito para testar se o prisma realmente consegue acessar o banco
-                    É importante rodar esse comando no início do projeto para ver se está dando tudo certo
-                    O sucesso da criação de uma tabela de teste mostra que a interação com o banco está acontecendo.
-                        OBS.: Esse comando de teste limpa tudo o que tem no banco, por isso ele deve ser rodada NO INÍCIO DO PROJETO!!!
 */
 
 // Importing libraries
@@ -38,6 +25,7 @@ const verifyUserLogin = require('../middleware/verifyUserLogin.js')
 app.use((request, response, next) => {
     response.header('Access-Control-Allow-Origin', '*')
     response.header('Access-Control-Allow-Methos', 'GET, POST, PUT, DELETE, OPTIONS')
+    response.header('Access-Control-Allow-Headers', 'Content-type')
 
     app.use(cors())
     next()
@@ -48,6 +36,7 @@ const jsonParser = express.json()
 
 // Exporting of routers
 const genreRouter = require('./routes/genreRoutes.js')
+const userRouter = require('./routes/userRoutes.js')
 
 const verifyJwt = async (request, response, next) => {
     let token = request.headers['x-access-token']
@@ -60,73 +49,7 @@ const verifyJwt = async (request, response, next) => {
     }
 }
 
-// Routes to user CRUD
-
-app.post('/user', cors(), jsonParser, async(req, res) => {
-    let statusCode
-    let message
-    let headerContentType
-
-    headerContentType = req.headers['content-type']
-
-    if(headerContentType == 'application/json') {
-        let bodyData = req.body
-
-        if(JSON.stringify(bodyData) != '{}') {
-            const newUser = await userController.newUser(bodyData)
-
-            statusCode = newUser.status
-            message = newUser.message
-        } else {
-            statusCode = 400
-            message = MESSAGE_ERROR.EMPTY_BODY
-        }
-    } else {
-        statusCode = 415
-        message = MESSAGE_ERROR.INCORRECT_CONTENT_TYPE
-    }
-
-    res.status(statusCode).json(message)
-})
-
-app.get('/users', cors(), async(req, res) => {
-    
-})
-
-app.get('/user/user-name/:userName', cors(), async(req, res) => {
-
-})
-
-app.get('/user/id/:userId', cors(), async(req, res) => {
-    let statusCode
-    let message
-    let id = req.params.userId
-
-    if(id != '' && id != undefined) {
-        const userData = await userController.searchUserByID(id)
-
-        if(userData) {
-            statusCode = userData.status
-            message = userData.message
-        } else {
-            statusCode = userData.status
-            message = userData.message
-        }
-    } else {
-        statusCode = userData.status
-        message = userData.message
-    }
-
-    res.status(statusCode).json(message)
-})
-
-app.put('/user/:userId', cors(), jsonParser, async(req, res) => {
-
-})
-
-app.delete('/user/:userId', cors(), jsonParser, async(req, res) => {
-
-})
+app.use(userRouter)
 
 app.post('/user/login', cors(), jsonParser, async(req, res) => {
     let statusCode
@@ -179,15 +102,7 @@ app.post('/user/login', cors(), jsonParser, async(req, res) => {
     }
 })
 
-// Routes to user CRUD
-
-/*******************************************************************/
-
-// Routes to genres (of books) CRUD
-
 app.use(genreRouter)
-
-// Routes to genres (of books) CRUD
 
 app.listen(3030, () => {
     console.log("Server waiting requests...")
---
+++ src/routes/userRoutes.js
@@ -0,0 +1,126 @@
+const express = require('express')
+const cors = require('cors')
+const jsonParser = express.json()
+const userController = require('../controllers/userController.js')
+
+// File with standardized messages
+const { MESSAGE_SUCCESS, MESSAGE_ERROR } = require('../module/config.js')
+
+const router = express.Router()
+
+router
+    .route('/user')
+    .post(jsonParser, async(req, res) => {
+        let statusCode
+        let message
+        let headerContentType
+    
+        headerContentType = req.headers['content-type']
+    
+        if(headerContentType == 'application/json') {
+            let bodyData = req.body
+    
+            if(JSON.stringify(bodyData) != '{}') {
+                const newUser = await userController.newUser(bodyData)
+    
+                statusCode = newUser.status
+                message = newUser.message
+            } else {
+                statusCode = 400
+                message = MESSAGE_ERROR.EMPTY_BODY
+            }
+        } else {
+            statusCode = 415
+            message = MESSAGE_ERROR.INCORRECT_CONTENT_TYPE
+        }
+    
+        res.status(statusCode).json(message)
+    })
+
+router
+    .route('/user/id/:userId')
+    .get(async(req, res) => {
+        let statusCode
+        let message
+        let id = req.params.userId
+    
+        if(id != '' && id != undefined) {
+            const userData = await userController.searchUserByID(id)
+    
+            if(userData) {
+                statusCode = userData.status
+                message = userData.message
+            } else {
+                statusCode = userData.status
+                message = userData.message
+            }
+        } else {
+            statusCode = userData.status
+            message = userData.message
+        }
+    
+        res.status(statusCode).json(message)
+    })
+
+// router
+//     .route('/user/login')
+//     .post(jsonParser, async(req, res) => {
+//     let statusCode
+//     let message
+//     let headerContentType
+
+//     headerContentType = req.headers['content-type']
+
+//     if(headerContentType == 'application/json') {
+//         let bodyData = req.body
+
+//         if(JSON.stringify(bodyData) != '{}') {
+//             const userLogin = await userController.userLogin(bodyData.user_name, bodyData.senha)
+
+//             if(userLogin.status == 200) {
+//                 userLogin.message.forEach(async (userInfo) => {
+//                     const authenticatedUser = await verifyUserLogin.verifyLogin(userInfo)
+
+//                     if(authenticatedUser) {
+//                         const createJwt = await jwt.createJwt(authenticatedUser)
+//                         statusCode = createJwt.status
+//                         message = createJwt.response
+
+//                         return res.status(statusCode).json(message)
+//                     } else {
+//                         statusCode = 401
+//                         message = MESSAGE_ERROR.INVALID_USER
+                        
+//                         return response.status(statusCode).json(message)
+//                     }
+//                 })
+//             } else {
+//                 statusCode = userLogin.status
+//                 message = userLogin.message
+
+//                 return res.status(statusCode).json(message)
+//             }
+
+//         } else {
+//             statusCode = 400
+//             message = MESSAGE_ERROR.EMPTY_BODY
+
+//             return res.status(statusCode).json(message)
+//         }
+//     } else {
+//         statusCode = 415
+//         message = MESSAGE_ERROR.INCORRECT_CONTENT_TYPE
+
+//         return res.status(statusCode).json(message)
+//     }
+// })
+
+module.exports = router
+
+// app.get('/users', cors(), async(req, res)
+
+// app.get('/user/user-name/:userName', cors(), async(req, res)
+
+// app.put('/user/:userId', cors(), jsonParser, async(req, res)
+
+// app.delete('/user/:userId', cors(), jsonParser, async(req, res)
