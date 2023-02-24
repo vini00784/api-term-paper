50f1e99a9511add517b0349def18f6562cc0d1e2
vini00784 <vo70346@gmail.com>
2023-02-24 09:46:42 -0300

    small changes

--- middleware/verifyUserLogin.js
+++ middleware/verifyUserLogin.js
@@ -1,10 +1,10 @@
 const userController = require('../src/controllers/userController.js')
 
 const verifyLogin = async (userInfos) => {
-    const { users } = await userController.listAllUsers()
+    const object = await userController.listAllUsers()
     let foundUser
     
-    users.forEach(user => {
+    object.message.users.forEach(user => {
         if(user.user_name == userInfos.user_name && user.senha == userInfos.senha) {
             foundUser = user
         }
