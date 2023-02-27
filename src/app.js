e1e3dd2b0432d34018e590201220da5ad2f1e3e8
vini00784 <vo70346@gmail.com>
2023-02-27 07:40:37 -0300

    refactoring user login route

--- src/app.js
+++ src/app.js
@@ -11,15 +11,8 @@ const express = require('express')
 const cors = require("cors")
 const app = express()
 
-// File with standardized messages
-const { MESSAGE_SUCCESS, MESSAGE_ERROR } = require('./module/config.js')
-
-// Import of controllers
-const userController = require('./controllers/userController.js')
-
 // Function to generate jwt
 const jwt = require('../middleware/jwt.js')
-const verifyUserLogin = require('../middleware/verifyUserLogin.js')
 
 // Cors configuration to release API access
 app.use((request, response, next) => {
@@ -30,9 +23,6 @@ app.use((request, response, next) => {
     app.use(cors())
     next()
 })
-    
-// Creating an object that allows you to receive a JSON in the body of requests
-const jsonParser = express.json()
 
 // Exporting of routers
 const genreRouter = require('./routes/genreRoutes.js')
@@ -49,60 +39,12 @@ const verifyJwt = async (request, response, next) => {
     }
 }
 
+// Using routers
+
 app.use(userRouter)
-
-app.post('/user/login', cors(), jsonParser, async(req, res) => {
-    let statusCode
-    let message
-    let headerContentType
-
-    headerContentType = req.headers['content-type']
-
-    if(headerContentType == 'application/json') {
-        let bodyData = req.body
-
-        if(JSON.stringify(bodyData) != '{}') {
-            const userLogin = await userController.userLogin(bodyData.user_name, bodyData.senha)
-
-            if(userLogin.status == 200) {
-                userLogin.message.forEach(async (userInfo) => {
-                    const authenticatedUser = await verifyUserLogin.verifyLogin(userInfo)
-
-                    if(authenticatedUser) {
-                        const createJwt = await jwt.createJwt(authenticatedUser)
-                        statusCode = createJwt.status
-                        message = createJwt.response
-
-                        return res.status(statusCode).json(message)
-                    } else {
-                        statusCode = 401
-                        message = MESSAGE_ERROR.INVALID_USER
-                        
-                        return response.status(statusCode).json(message)
-                    }
-                })
-            } else {
-                statusCode = userLogin.status
-                message = userLogin.message
-
-                return res.status(statusCode).json(message)
-            }
-
-        } else {
-            statusCode = 400
-            message = MESSAGE_ERROR.EMPTY_BODY
-
-            return res.status(statusCode).json(message)
-        }
-    } else {
-        statusCode = 415
-        message = MESSAGE_ERROR.INCORRECT_CONTENT_TYPE
-
-        return res.status(statusCode).json(message)
-    }
-})
-
 app.use(genreRouter)
+
+// Using routers
 
 app.listen(3030, () => {
     console.log("Server waiting requests...")
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,7 +1,8 @@
 const express = require('express')
-const cors = require('cors')
 const jsonParser = express.json()
 const userController = require('../controllers/userController.js')
+const verifyUserLogin = require('../../middleware/verifyUserLogin.js')
+const jwt = require('../../middleware/jwt.js')
 
 // File with standardized messages
 const { MESSAGE_SUCCESS, MESSAGE_ERROR } = require('../module/config.js')
@@ -62,58 +63,58 @@ router
         res.status(statusCode).json(message)
     })
 
-// router
-//     .route('/user/login')
-//     .post(jsonParser, async(req, res) => {
-//     let statusCode
-//     let message
-//     let headerContentType
+router
+    .route('/user/login')
+    .post(jsonParser, async(req, res) => {
+    let statusCode
+    let message
+    let headerContentType
 
-//     headerContentType = req.headers['content-type']
+    headerContentType = req.headers['content-type']
 
-//     if(headerContentType == 'application/json') {
-//         let bodyData = req.body
+    if(headerContentType == 'application/json') {
+        let bodyData = req.body
 
-//         if(JSON.stringify(bodyData) != '{}') {
-//             const userLogin = await userController.userLogin(bodyData.user_name, bodyData.senha)
+        if(JSON.stringify(bodyData) != '{}') {
+            const userLogin = await userController.userLogin(bodyData.user_name, bodyData.senha)
 
-//             if(userLogin.status == 200) {
-//                 userLogin.message.forEach(async (userInfo) => {
-//                     const authenticatedUser = await verifyUserLogin.verifyLogin(userInfo)
+            if(userLogin.status == 200) {
+                userLogin.message.forEach(async (userInfo) => {
+                    const authenticatedUser = await verifyUserLogin.verifyLogin(userInfo)
 
-//                     if(authenticatedUser) {
-//                         const createJwt = await jwt.createJwt(authenticatedUser)
-//                         statusCode = createJwt.status
-//                         message = createJwt.response
+                    if(authenticatedUser) {
+                        const createJwt = await jwt.createJwt(authenticatedUser)
+                        statusCode = createJwt.status
+                        message = createJwt.response
 
-//                         return res.status(statusCode).json(message)
-//                     } else {
-//                         statusCode = 401
-//                         message = MESSAGE_ERROR.INVALID_USER
+                        return res.status(statusCode).json(message)
+                    } else {
+                        statusCode = 401
+                        message = MESSAGE_ERROR.INVALID_USER
                         
-//                         return response.status(statusCode).json(message)
-//                     }
-//                 })
-//             } else {
-//                 statusCode = userLogin.status
-//                 message = userLogin.message
+                        return response.status(statusCode).json(message)
+                    }
+                })
+            } else {
+                statusCode = userLogin.status
+                message = userLogin.message
 
-//                 return res.status(statusCode).json(message)
-//             }
+                return res.status(statusCode).json(message)
+            }
 
-//         } else {
-//             statusCode = 400
-//             message = MESSAGE_ERROR.EMPTY_BODY
+        } else {
+            statusCode = 400
+            message = MESSAGE_ERROR.EMPTY_BODY
 
-//             return res.status(statusCode).json(message)
-//         }
-//     } else {
-//         statusCode = 415
-//         message = MESSAGE_ERROR.INCORRECT_CONTENT_TYPE
+            return res.status(statusCode).json(message)
+        }
+    } else {
+        statusCode = 415
+        message = MESSAGE_ERROR.INCORRECT_CONTENT_TYPE
 
-//         return res.status(statusCode).json(message)
-//     }
-// })
+        return res.status(statusCode).json(message)
+    }
+})
 
 module.exports = router
 
