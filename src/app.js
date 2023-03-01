ae5408205d4e8b41e9ff9f74bf1dc4d35b4ca94d
vini00784 <vo70346@gmail.com>
2023-03-01 08:26:10 -0300

    moving function to verify jwt

--- src/app.js
+++ src/app.js
@@ -11,9 +11,6 @@ const express = require('express')
 const cors = require("cors")
 const app = express()
 
-// Function to generate jwt
-const jwt = require('../middleware/jwt.js')
-
 // Cors configuration to release API access
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*')
@@ -27,17 +24,6 @@ app.use((req, res, next) => {
 // Importing of routers
 const genreRouter = require('./routes/genreRoutes.js')
 const userRouter = require('./routes/userRoutes.js')
-
-const verifyJwt = async (req, res, next) => {
-    let token = req.headers['x-access-token']
-    const authenticatedToken = await jwt.validateJwt(token)
-
-    if(authenticatedToken) {
-        next()
-    } else {
-        return res.status(401).end()
-    }
-}
 
 // Using routers
 
