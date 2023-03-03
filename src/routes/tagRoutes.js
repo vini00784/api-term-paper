81efe1ee6250fce7a052600d1f52d0cebe684829
vini00784 <vo70346@gmail.com>
2023-03-03 11:01:53 -0300

    routes to manipulate tags data

---
+++ src/routes/tagRoutes.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const jsonParser = express.json()
+const tagController = require('../controllers/tagController.js')
+
+// File with standardized messages
+const { MESSAGE_SUCCESS, MESSAGE_ERROR } = require('../module/config.js')
+
+const router = express.Router()
+
+router
+    .route('/tags/id/:userId')
+    .get(async(req, res) => {
+        let statusCode
+        let message
+        let id = req.params.userId
+
+        if(id != '' && id != undefined) {
+            const tagsData = await tagController.listTagByUserId(id)
+
+            if(tagsData) {
+                statusCode = tagsData.status
+                message = tagsData.message
+            } else {
+                statusCode = 404
+                message = MESSAGE_ERROR.NOT_FOUND_DB
+            }
+        } else {
+            statusCode = 400
+            message = MESSAGE_ERROR.REQUIRED_ID
+        }
+
+        res.status(statusCode).json(message)
+    })
+
+module.exports = router
