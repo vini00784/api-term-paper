3e3282a85d0b6571b85ea4560ad0a973ffa1918d
vini00784 <vo70346@gmail.com>
2023-03-13 11:24:49 -0300

    small changes in function return

--- src/controllers/genreController.js
+++ src/controllers/genreController.js
@@ -12,13 +12,10 @@ const { MESSAGE_SUCCESS, MESSAGE_ERROR } = require('../module/config.js')
 const genreModel = require('../models/DAO/genre.js')
 
 exports.listAllGenres = async () => {
-    let genresJson = {}
-
     const genresData = await genreModel.selectAllGenres()
 
     if(genresData) {
-        genresJson.genres = genresData
-        return {status: 200, message: genresJson}
+        return {status: 200, message: genresData}
     } else
         return {status: 404, message: MESSAGE_ERROR.NOT_FOUND_DB}
 }
