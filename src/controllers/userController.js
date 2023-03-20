f8cecba6447dee03264d2071e055ced58b871901
vini00784 <vo70346@gmail.com>
2023-03-20 10:54:49 -0300

    small changes

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -18,11 +18,11 @@ const userTagModel = require('../models/DAO/userTag.js')
 const userGenreModel = require('../models/DAO/userGenre.js')
 
 const newUser = async (user) => {
-    if(user.user_name == '' || user.user_name == undefined || user.nome == '' || user.nome == undefined || user.data_nascimento == ''|| user.data_nascimento == undefined || user.email == '' || user.email == undefined || user.senha == ''|| user.senha == undefined)
+    if(user.user_name == '' || user.user_name == undefined || user.nome == '' || user.nome == undefined || user.data_nascimento == ''|| user.data_nascimento == undefined || user.email == '' || user.email == undefined || user.uid == ''|| user.uid == undefined)
         return { status: 400, message: MESSAGE_ERROR.REQUIRED_FIELDS }
     else if(!user.email.includes('@'))
         return { status: 400, message: MESSAGE_ERROR.INVALID_EMAIL }
-    else if(user.user_name.length > 30 || user.nome.length > 200 || user.email.length > 256 || user.senha.length > 100)
+    else if(user.user_name.length > 30 || user.nome.length > 200 || user.email.length > 256 || user.uid.length > 100)
         return { status: 400, message: MESSAGE_ERROR.EXCEEDED_CHARACTERS }
     else {
         user.premium = 0
