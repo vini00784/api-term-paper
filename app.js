3270b6946afc7a2d9143f9364275e307160a55ed
vini00784 <vo70346@gmail.com>
2023-04-20 09:30:51 -0300

    changing port

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ app.get('/', (req, res) => {
     })
 })
 
-const port = process.env.PORT || 3030
+const port = process.env.PORT || 8080
 
 app.listen(port, () => {
     console.log("Server waiting requests...")
